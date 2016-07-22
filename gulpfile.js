f83b95fbea4796008e426c58a2ef9e023ebaae17
David <davidbasto@gmail.com>
2016-07-22 19:55:13 -0300

    Commit incial

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var es = require('event-stream');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
+var runSequence = require('run-sequence');
+var rename = require('gulp-rename');
+var ngAnnotate = require('gulp-ng-annotate');
+
+gulp.task('clean', function() {
+	return gulp.src('dist/')
+		.pipe(clean());
+});
+
+gulp.task('jshint', function() {
+	return gulp.src('js/**/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('uglify', function() {
+	return es.merge([
+		gulp.src(['components/angular/angular.min.js',
+				  'components/angular/angular-route.min.js',
+				  'components/angular/angular-messages.min.js']),
+		gulp.src(['lib/**/*.js', 'js/**/*.js'])
+			.pipe(concat('scripts.js'))
+			.pipe(ngAnnotate())
+			.pipe(uglify())
+		])
+		.pipe(concat('all.min.js'))
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('htmlmin', function() {
+	return gulp.src('view/*.html')
+	.pipe(htmlmin({collapseWhitespace: true}))
+	.pipe(gulp.dest('dist/view'))
+});
+
+gulp.task('cssmin', function() {
+	return gulp.src(['components/bootstrap/bootstrap.css', 'css/**/*.css'])
+		.pipe(cleanCSS())
+		.pipe(concat('styles.min.css'))
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('copy', function() {
+	return gulp.src('index-prod.html')
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest('dist/'))
+});
+
+gulp.task('default', function(cb) {
+	return runSequence('clean', ['jshint', 'uglify', 'htmlmin', 'cssmin', 'copy'], cb);
+}); 
